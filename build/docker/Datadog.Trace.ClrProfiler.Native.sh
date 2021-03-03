DataDog/dd-trace-dotnet

Switch integration tests to release (#1212)
* Switch integration tests to release

* Fix build configuration

* Fix runner pipeline

build/docker/Datadog.Trace.ClrProfiler.Native.sh
@@ -12,11 +12,11 @@ cd src/Datadog.Trace.ClrProfiler.Native
 mkdir -p build
 (cd build && cmake ../ && make)
 
-mkdir -p bin/Debug/x64
-cp -f build/bin/Datadog.Trace.ClrProfiler.Native.so bin/Debug/x64/Datadog.Trace.ClrProfiler.Native.so
+mkdir -p bin/Release/x64
+cp -f build/bin/Datadog.Trace.ClrProfiler.Native.so bin/Release/x64/Datadog.Trace.ClrProfiler.Native.so
 
-mkdir -p bin/Debug/x64/netstandard2.0
-cp -f $PUBLISH_OUTPUT_NET2/*.dll bin/Debug/x64/netstandard2.0/
+mkdir -p bin/Release/x64/netstandard2.0
+cp -f $PUBLISH_OUTPUT_NET2/*.dll bin/Release/x64/netstandard2.0/
 
-mkdir -p bin/Debug/x64/netcoreapp3.1
-cp -f $PUBLISH_OUTPUT_NET31/*.dll bin/Debug/x64/netcoreapp3.1/
+mkdir -p bin/Release/x64/netcoreapp3.1
+cp -f $PUBLISH_OUTPUT_NET31/*.dll bin/Release/x64/netcoreapp3.1/

pjanotti/opentelemetry-dotnet-instrumentation

build/docker/Datadog.Trace.ClrProfiler.Native.sh
@@ -12,11 +12,11 @@ cd src/Datadog.Trace.ClrProfiler.Native
 mkdir -p build
 (cd build && cmake ../ && make)
 
-mkdir -p bin/Debug/x64
-cp -f build/bin/Datadog.Trace.ClrProfiler.Native.so bin/Debug/x64/OpenTelemetry.AutoInstrumentation.ClrProfiler.Native.so
+mkdir -p bin/Release/x64
+cp -f build/bin/Datadog.Trace.ClrProfiler.Native.so bin/Release/x64/OpenTelemetry.AutoInstrumentation.ClrProfiler.Native.so
 
-mkdir -p bin/Debug/x64/netstandard2.0
-cp -f $PUBLISH_OUTPUT_NET2/*.dll bin/Debug/x64/netstandard2.0/
+mkdir -p bin/Release/x64/netstandard2.0
+cp -f $PUBLISH_OUTPUT_NET2/*.dll bin/Release/x64/netstandard2.0/
 
-mkdir -p bin/Debug/x64/netcoreapp3.1
-cp -f $PUBLISH_OUTPUT_NET31/*.dll bin/Debug/x64/netcoreapp3.1/
+mkdir -p bin/Release/x64/netcoreapp3.1
+cp -f $PUBLISH_OUTPUT_NET31/*.dll bin/Release/x64/netcoreapp3.1/