DataDog/dd-trace-dotnet

CallTarget Integration: ADO.NET MySqlData client version 6 (#1184)
* Add MySqlData support and execute the tests in multiple client versions.

* Fix Samples.MySql.csproj project.

* Add old MySql server for older clients test.

* Fix StartsWith on .NET frameworks.

* Fix Sample.MySql project

* Fix the test for callsite instrumentation in 6.8.8 client version.

* change sample app connection string

* fix connection string param name.

* Improves the csproj file conditions.

* Remove code for debugging

* update integrations.json

test/Datadog.Trace.ClrProfiler.IntegrationTests/AdoNet/MySqlCommandTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using Datadog.Core.Tools;
 using Datadog.Trace.Configuration;
@@ -15,59 +16,51 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.AdoNet
             SetServiceVersion("1.0.0");
         }
 
-        [Theory]
-        [InlineData(false, false)]
-        [InlineData(true, false)]
-        [InlineData(true, true)]
-        [Trait("Category", "EndToEnd")]
-        public void SubmitsTracesWithNetStandard(bool enableCallTarget, bool enableInlining)
+        public static IEnumerable<object[]> GetMySql8Data()
         {
-            SetCallTargetSettings(enableCallTarget, enableInlining);
-
-            // Note: The automatic instrumentation currently bails out on the generic wrappers.
-            // Once this is implemented, this will add another 1 group for the direct assembly reference
-            // and another 1 group for the netstandard assembly reference
-#if NET452
-            var expectedSpanCount = 50; // 7 queries * 7 groups + 1 internal query
-#else
-            var expectedSpanCount = 78; // 7 queries * 11 groups + 1 internal query
-#endif
-
-            if (enableCallTarget)
+            foreach (object[] item in PackageVersions.MySqlData)
             {
-#if NET452
-                expectedSpanCount = 62;
-#else
-                expectedSpanCount = 97;
-#endif
-            }
-
-            const string dbType = "mysql";
-            const string expectedOperationName = dbType + ".query";
-            const string expectedServiceName = "Samples.MySql-" + dbType;
-
-            // NOTE: opt into the additional instrumentation of calls into netstandard.dll
-            SetEnvironmentVariable("DD_TRACE_NETSTANDARD_ENABLED", "true");
-
-            int agentPort = TcpPortProvider.GetOpenPort();
-
-            using (var agent = new MockTracerAgent(agentPort))
-            using (ProcessResult processResult = RunSampleAndWaitForExit(agent.Port))
-            {
-                Assert.True(processResult.ExitCode >= 0, $"Process exited with code {processResult.ExitCode}");
-
-                var spans = agent.WaitForSpans(expectedSpanCount, operationName: expectedOperationName);
-                Assert.Equal(expectedSpanCount, spans.Count);
-
-                foreach (var span in spans)
+                if ((string)item[0] == string.Empty || !((string)item[0]).StartsWith("8"))
                 {
-                    Assert.Equal(expectedOperationName, span.Name);
-                    Assert.Equal(expectedServiceName, span.Service);
-                    Assert.Equal(SpanTypes.Sql, span.Type);
-                    Assert.Equal(dbType, span.Tags[Tags.DbType]);
-                    Assert.False(span.Tags?.ContainsKey(Tags.Version), "External service span should not have service version tag.");
+                    continue;
                 }
+
+                yield return item.Concat(new object[] { false, false, }).ToArray();
+                yield return item.Concat(new object[] { true, false, }).ToArray();
+                yield return item.Concat(new object[] { true, true, }).ToArray();
             }
+        }
+
+        public static IEnumerable<object[]> GetOldMySqlData()
+        {
+            foreach (object[] item in PackageVersions.MySqlData)
+            {
+                if ((string)item[0] == string.Empty || ((string)item[0]).StartsWith("8"))
+                {
+                    continue;
+                }
+
+                yield return item.Concat(new object[] { false, false, }).ToArray();
+                yield return item.Concat(new object[] { true, false, }).ToArray();
+                yield return item.Concat(new object[] { true, true, }).ToArray();
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(GetMySql8Data))]
+        [Trait("Category", "EndToEnd")]
+        public void SubmitsTracesWithNetStandardInMySql8(string packageVersion, bool enableCallTarget, bool enableInlining)
+        {
+            SubmitsTracesWithNetStandard(packageVersion, enableCallTarget, enableInlining);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetOldMySqlData))]
+        [Trait("Category", "EndToEnd")]
+        [Trait("Category", "ArmUnsupported")]
+        public void SubmitsTracesWithNetStandardInOldMySql(string packageVersion, bool enableCallTarget, bool enableInlining)
+        {
+            SubmitsTracesWithNetStandard(packageVersion, enableCallTarget, enableInlining);
         }
 
         [Theory]
@@ -96,6 +89,60 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.AdoNet
                 var spans = agent.WaitForSpans(totalSpanCount, returnAllOperations: true);
                 Assert.NotEmpty(spans);
                 Assert.Empty(spans.Where(s => s.Name.Equals(expectedOperationName)));
+            }
+        }
+
+        private void SubmitsTracesWithNetStandard(string packageVersion, bool enableCallTarget, bool enableInlining)
+        {
+            SetCallTargetSettings(enableCallTarget, enableInlining);
+
+            // Note: The automatic instrumentation currently bails out on the generic wrappers.
+            // Once this is implemented, this will add another 1 group for the direct assembly reference
+            // and another 1 group for the netstandard assembly reference
+#if NET452
+            var expectedSpanCount = 50; // 7 queries * 7 groups + 1 internal query
+#else
+            var expectedSpanCount = 78; // 7 queries * 11 groups + 1 internal query
+            if (packageVersion == "6.8.8")
+            {
+                expectedSpanCount = 76; // For this version the callsite instrumentation returns 2 spans less.
+            }
+#endif
+
+            if (enableCallTarget)
+            {
+#if NET452
+                expectedSpanCount = 62;
+#else
+                expectedSpanCount = 97;
+#endif
+            }
+
+            const string dbType = "mysql";
+            const string expectedOperationName = dbType + ".query";
+            const string expectedServiceName = "Samples.MySql-" + dbType;
+
+            // NOTE: opt into the additional instrumentation of calls into netstandard.dll
+            SetEnvironmentVariable("DD_TRACE_NETSTANDARD_ENABLED", "true");
+
+            int agentPort = TcpPortProvider.GetOpenPort();
+
+            using (var agent = new MockTracerAgent(agentPort))
+            using (ProcessResult processResult = RunSampleAndWaitForExit(agent.Port, packageVersion: packageVersion))
+            {
+                Assert.True(processResult.ExitCode >= 0, $"Process exited with code {processResult.ExitCode}");
+
+                var spans = agent.WaitForSpans(expectedSpanCount, operationName: expectedOperationName);
+                Assert.Equal(expectedSpanCount, spans.Count);
+
+                foreach (var span in spans)
+                {
+                    Assert.Equal(expectedOperationName, span.Name);
+                    Assert.Equal(expectedServiceName, span.Service);
+                    Assert.Equal(SpanTypes.Sql, span.Type);
+                    Assert.Equal(dbType, span.Tags[Tags.DbType]);
+                    Assert.False(span.Tags?.ContainsKey(Tags.Version), "External service span should not have service version tag.");
+                }
             }
         }
     }

pjanotti/opentelemetry-dotnet-instrumentation

test/Datadog.Trace.ClrProfiler.IntegrationTests/AdoNet/MySqlCommandTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using Datadog.Core.Tools;
 using Datadog.Trace.Configuration;
@@ -15,59 +16,51 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.AdoNet
             SetServiceVersion("1.0.0");
         }
 
-        [Theory]
-        [InlineData(false, false)]
-        [InlineData(true, false)]
-        [InlineData(true, true)]
-        [Trait("Category", "EndToEnd")]
-        public void SubmitsTracesWithNetStandard(bool enableCallTarget, bool enableInlining)
+        public static IEnumerable<object[]> GetMySql8Data()
         {
-            SetCallTargetSettings(enableCallTarget, enableInlining);
-
-            // Note: The automatic instrumentation currently bails out on the generic wrappers.
-            // Once this is implemented, this will add another 1 group for the direct assembly reference
-            // and another 1 group for the netstandard assembly reference
-#if NET452
-            var expectedSpanCount = 50; // 7 queries * 7 groups + 1 internal query
-#else
-            var expectedSpanCount = 78; // 7 queries * 11 groups + 1 internal query
-#endif
-
-            if (enableCallTarget)
+            foreach (object[] item in PackageVersions.MySqlData)
             {
-#if NET452
-                expectedSpanCount = 62;
-#else
-                expectedSpanCount = 97;
-#endif
-            }
-
-            const string dbType = "mysql";
-            const string expectedOperationName = dbType + ".query";
-            const string expectedServiceName = "Samples.MySql-" + dbType;
-
-            // NOTE: opt into the additional instrumentation of calls into netstandard.dll
-            SetEnvironmentVariable("OTEL_TRACE_NETSTANDARD_ENABLED", "true");
-
-            int agentPort = TcpPortProvider.GetOpenPort();
-
-            using (var agent = new MockTracerAgent(agentPort))
-            using (ProcessResult processResult = RunSampleAndWaitForExit(agent.Port))
-            {
-                Assert.True(processResult.ExitCode >= 0, $"Process exited with code {processResult.ExitCode}");
-
-                var spans = agent.WaitForSpans(expectedSpanCount, operationName: expectedOperationName);
-                Assert.Equal(expectedSpanCount, spans.Count);
-
-                foreach (var span in spans)
+                if ((string)item[0] == string.Empty || !((string)item[0]).StartsWith("8"))
                 {
-                    Assert.Equal(expectedOperationName, span.Name);
-                    Assert.Equal(expectedServiceName, span.Service);
-                    Assert.Equal(SpanTypes.Sql, span.Type);
-                    Assert.Equal(dbType, span.Tags[Tags.DbType]);
-                    Assert.False(span.Tags?.ContainsKey(Tags.Version), "External service span should not have service version tag.");
+                    continue;
                 }
+
+                yield return item.Concat(new object[] { false, false, }).ToArray();
+                yield return item.Concat(new object[] { true, false, }).ToArray();
+                yield return item.Concat(new object[] { true, true, }).ToArray();
             }
+        }
+
+        public static IEnumerable<object[]> GetOldMySqlData()
+        {
+            foreach (object[] item in PackageVersions.MySqlData)
+            {
+                if ((string)item[0] == string.Empty || ((string)item[0]).StartsWith("8"))
+                {
+                    continue;
+                }
+
+                yield return item.Concat(new object[] { false, false, }).ToArray();
+                yield return item.Concat(new object[] { true, false, }).ToArray();
+                yield return item.Concat(new object[] { true, true, }).ToArray();
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(GetMySql8Data))]
+        [Trait("Category", "EndToEnd")]
+        public void SubmitsTracesWithNetStandardInMySql8(string packageVersion, bool enableCallTarget, bool enableInlining)
+        {
+            SubmitsTracesWithNetStandard(packageVersion, enableCallTarget, enableInlining);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetOldMySqlData))]
+        [Trait("Category", "EndToEnd")]
+        [Trait("Category", "ArmUnsupported")]
+        public void SubmitsTracesWithNetStandardInOldMySql(string packageVersion, bool enableCallTarget, bool enableInlining)
+        {
+            SubmitsTracesWithNetStandard(packageVersion, enableCallTarget, enableInlining);
         }
 
         [Theory]
@@ -96,6 +89,60 @@ namespace Datadog.Trace.ClrProfiler.IntegrationTests.AdoNet
                 var spans = agent.WaitForSpans(totalSpanCount, returnAllOperations: true);
                 Assert.NotEmpty(spans);
                 Assert.Empty(spans.Where(s => s.Name.Equals(expectedOperationName)));
+            }
+        }
+
+        private void SubmitsTracesWithNetStandard(string packageVersion, bool enableCallTarget, bool enableInlining)
+        {
+            SetCallTargetSettings(enableCallTarget, enableInlining);
+
+            // Note: The automatic instrumentation currently bails out on the generic wrappers.
+            // Once this is implemented, this will add another 1 group for the direct assembly reference
+            // and another 1 group for the netstandard assembly reference
+#if NET452
+            var expectedSpanCount = 50; // 7 queries * 7 groups + 1 internal query
+#else
+            var expectedSpanCount = 78; // 7 queries * 11 groups + 1 internal query
+            if (packageVersion == "6.8.8")
+            {
+                expectedSpanCount = 76; // For this version the callsite instrumentation returns 2 spans less.
+            }
+#endif
+
+            if (enableCallTarget)
+            {
+#if NET452
+                expectedSpanCount = 62;
+#else
+                expectedSpanCount = 97;
+#endif
+            }
+
+            const string dbType = "mysql";
+            const string expectedOperationName = dbType + ".query";
+            const string expectedServiceName = "Samples.MySql-" + dbType;
+
+            // NOTE: opt into the additional instrumentation of calls into netstandard.dll
+            SetEnvironmentVariable("OTEL_TRACE_NETSTANDARD_ENABLED", "true");
+
+            int agentPort = TcpPortProvider.GetOpenPort();
+
+            using (var agent = new MockTracerAgent(agentPort))
+            using (ProcessResult processResult = RunSampleAndWaitForExit(agent.Port, packageVersion: packageVersion))
+            {
+                Assert.True(processResult.ExitCode >= 0, $"Process exited with code {processResult.ExitCode}");
+
+                var spans = agent.WaitForSpans(expectedSpanCount, operationName: expectedOperationName);
+                Assert.Equal(expectedSpanCount, spans.Count);
+
+                foreach (var span in spans)
+                {
+                    Assert.Equal(expectedOperationName, span.Name);
+                    Assert.Equal(expectedServiceName, span.Service);
+                    Assert.Equal(SpanTypes.Sql, span.Type);
+                    Assert.Equal(dbType, span.Tags[Tags.DbType]);
+                    Assert.False(span.Tags?.ContainsKey(Tags.Version), "External service span should not have service version tag.");
+                }
             }
         }
     }