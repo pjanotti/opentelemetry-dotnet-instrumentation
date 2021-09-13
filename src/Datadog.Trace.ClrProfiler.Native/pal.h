DataDog/dd-trace-dotnet

Change native format to Microsoft style (#1544)
* clang-format with Microsoft style

* Improve formatting

src/Datadog.Trace.ClrProfiler.Native/pal.h
@@ -3,14 +3,14 @@
 
 #ifdef _WIN32
 
-#include <process.h>
-#include <filesystem>
 #include "windows.h"
+#include <filesystem>
+#include <process.h>
 
 #else
 
-#include <unistd.h>
 #include <fstream>
+#include <unistd.h>
 
 #endif
 
@@ -19,80 +19,80 @@
 #endif
 
 #include "environment_variables.h"
-#include "string.h"  // NOLINT
+#include "string.h" // NOLINT
 #include "util.h"
 
 namespace trace {
 
-inline WSTRING DatadogLogFilePath(const std::string& file_name_suffix) {
-  WSTRING directory = GetEnvironmentValue(environment::log_directory);
+inline WSTRING DatadogLogFilePath(const std::string& file_name_suffix)
+{
+    WSTRING directory = GetEnvironmentValue(environment::log_directory);
 
-  if (directory.length() > 0) {
-    return directory +
+    if (directory.length() > 0) {
+        return directory +
 #ifdef _WIN32
-           WStr('\\') +
+               WStr('\\') +
 #else
-           WStr('/') +
+               WStr('/') +
 #endif
-           ToWSTRING("dotnet-tracer-native" + file_name_suffix + ".log");
-  }
+               ToWSTRING("dotnet-tracer-native" + file_name_suffix + ".log");
+    }
 
-  WSTRING path = GetEnvironmentValue(environment::log_path);
+    WSTRING path = GetEnvironmentValue(environment::log_path);
 
-  if (path.length() > 0) {
-    return path;
-  }
+    if (path.length() > 0) {
+        return path;
+    }
 
 #ifdef _WIN32
-  char* p_program_data;
-  size_t length;
-  const errno_t result = _dupenv_s(&p_program_data, &length, "PROGRAMDATA");
-  std::string program_data;
+    char* p_program_data;
+    size_t length;
+    const errno_t result = _dupenv_s(&p_program_data, &length, "PROGRAMDATA");
+    std::string program_data;
 
-  if (SUCCEEDED(result) && p_program_data != nullptr && length > 0) {
-    program_data = std::string(p_program_data);
-  } else {
-    program_data = R"(C:\ProgramData)";
-  }
+    if (SUCCEEDED(result) && p_program_data != nullptr && length > 0) {
+        program_data = std::string(p_program_data);
+    } else {
+        program_data = R"(C:\ProgramData)";
+    }
 
-  return ToWSTRING(program_data +
-                   R"(\Datadog .NET Tracer\logs\dotnet-tracer-native)" + 
-                   file_name_suffix + ".log");
+    return ToWSTRING(program_data + R"(\Datadog .NET Tracer\logs\dotnet-tracer-native)" + file_name_suffix + ".log");
 #else
-  return ToWSTRING("/var/log/datadog/dotnet/dotnet-tracer-native" +
-                   file_name_suffix + ".log");
+    return ToWSTRING("/var/log/datadog/dotnet/dotnet-tracer-native" + file_name_suffix + ".log");
 #endif
 }
 
-inline WSTRING GetCurrentProcessName() {
+inline WSTRING GetCurrentProcessName()
+{
 #ifdef _WIN32
-  const DWORD length = 260;
-  WCHAR buffer[length]{};
+    const DWORD length = 260;
+    WCHAR buffer[length]{};
 
-  const DWORD len = GetModuleFileName(nullptr, buffer, length);
-  const WSTRING current_process_path(buffer);
-  return std::filesystem::path(current_process_path).filename();
+    const DWORD len = GetModuleFileName(nullptr, buffer, length);
+    const WSTRING current_process_path(buffer);
+    return std::filesystem::path(current_process_path).filename();
 #elif MACOS
-  const int length = 260;
-  char* buffer = new char[length];
-  proc_name(getpid(), buffer, length);
-  return ToWSTRING(std::string(buffer));
+    const int length = 260;
+    char* buffer = new char[length];
+    proc_name(getpid(), buffer, length);
+    return ToWSTRING(std::string(buffer));
 #else
-  std::fstream comm("/proc/self/comm");
-  std::string name;
-  std::getline(comm, name);
-  return ToWSTRING(name);
+    std::fstream comm("/proc/self/comm");
+    std::string name;
+    std::getline(comm, name);
+    return ToWSTRING(name);
 #endif
 }
 
-inline int GetPID() {
+inline int GetPID()
+{
 #ifdef _WIN32
-  return _getpid();
+    return _getpid();
 #else
-  return getpid();
+    return getpid();
 #endif
 }
 
 } // namespace trace
 
-#endif  // DD_CLR_PROFILER_PAL_H_
+#endif // DD_CLR_PROFILER_PAL_H_

pjanotti/opentelemetry-dotnet-instrumentation

src/Datadog.Trace.ClrProfiler.Native/pal.h
@@ -3,14 +3,14 @@
 
 #ifdef _WIN32
 
-#include <process.h>
-#include <filesystem>
 #include "windows.h"
+#include <filesystem>
+#include <process.h>
 
 #else
 
-#include <unistd.h>
 #include <fstream>
+#include <unistd.h>
 
 #endif
 
@@ -19,80 +19,80 @@
 #endif
 
 #include "environment_variables.h"
-#include "string.h"  // NOLINT
+#include "string.h" // NOLINT
 #include "util.h"
 
 namespace trace {
 
-inline WSTRING DatadogLogFilePath(const std::string& file_name_suffix) {
-  WSTRING directory = GetEnvironmentValue(environment::log_directory);
+inline WSTRING DatadogLogFilePath(const std::string& file_name_suffix)
+{
+    WSTRING directory = GetEnvironmentValue(environment::log_directory);
 
-  if (directory.length() > 0) {
-    return directory +
+    if (directory.length() > 0) {
+        return directory +
 #ifdef _WIN32
-           WStr('\\') +
+               WStr('\\') +
 #else
-           WStr('/') +
+               WStr('/') +
 #endif
-           ToWSTRING("dotnet-tracer-native" + file_name_suffix + ".log");
-  }
+               ToWSTRING("dotnet-tracer-native" + file_name_suffix + ".log");
+    }
 
-  WSTRING path = GetEnvironmentValue(environment::log_path);
+    WSTRING path = GetEnvironmentValue(environment::log_path);
 
-  if (path.length() > 0) {
-    return path;
-  }
+    if (path.length() > 0) {
+        return path;
+    }
 
 #ifdef _WIN32
-  char* p_program_data;
-  size_t length;
-  const errno_t result = _dupenv_s(&p_program_data, &length, "PROGRAMDATA");
-  std::string program_data;
+    char* p_program_data;
+    size_t length;
+    const errno_t result = _dupenv_s(&p_program_data, &length, "PROGRAMDATA");
+    std::string program_data;
 
-  if (SUCCEEDED(result) && p_program_data != nullptr && length > 0) {
-    program_data = std::string(p_program_data);
-  } else {
-    program_data = R"(C:\ProgramData)";
-  }
+    if (SUCCEEDED(result) && p_program_data != nullptr && length > 0) {
+        program_data = std::string(p_program_data);
+    } else {
+        program_data = R"(C:\ProgramData)";
+    }
 
-  return ToWSTRING(program_data +
-                   R"(\OpenTelemetry .NET AutoInstrumentation\logs\dotnet-tracer-native)" + 
-                   file_name_suffix + ".log");
+    return ToWSTRING(program_data + R"(\OpenTelemetry .NET AutoInstrumentation\logs\dotnet-tracer-native)" + file_name_suffix + ".log");
 #else
-  return ToWSTRING("/var/log/opentelemetry/dotnet/dotnet-tracer-native" +
-                   file_name_suffix + ".log");
+    return ToWSTRING("/var/log/opentelemetry/dotnet/dotnet-tracer-native" + file_name_suffix + ".log");
 #endif
 }
 
-inline WSTRING GetCurrentProcessName() {
+inline WSTRING GetCurrentProcessName()
+{
 #ifdef _WIN32
-  const DWORD length = 260;
-  WCHAR buffer[length]{};
+    const DWORD length = 260;
+    WCHAR buffer[length]{};
 
-  const DWORD len = GetModuleFileName(nullptr, buffer, length);
-  const WSTRING current_process_path(buffer);
-  return std::filesystem::path(current_process_path).filename();
+    const DWORD len = GetModuleFileName(nullptr, buffer, length);
+    const WSTRING current_process_path(buffer);
+    return std::filesystem::path(current_process_path).filename();
 #elif MACOS
-  const int length = 260;
-  char* buffer = new char[length];
-  proc_name(getpid(), buffer, length);
-  return ToWSTRING(std::string(buffer));
+    const int length = 260;
+    char* buffer = new char[length];
+    proc_name(getpid(), buffer, length);
+    return ToWSTRING(std::string(buffer));
 #else
-  std::fstream comm("/proc/self/comm");
-  std::string name;
-  std::getline(comm, name);
-  return ToWSTRING(name);
+    std::fstream comm("/proc/self/comm");
+    std::string name;
+    std::getline(comm, name);
+    return ToWSTRING(name);
 #endif
 }
 
-inline int GetPID() {
+inline int GetPID()
+{
 #ifdef _WIN32
-  return _getpid();
+    return _getpid();
 #else
-  return getpid();
+    return getpid();
 #endif
 }
 
 } // namespace trace
 
-#endif  // DD_CLR_PROFILER_PAL_H_
+#endif // DD_CLR_PROFILER_PAL_H_