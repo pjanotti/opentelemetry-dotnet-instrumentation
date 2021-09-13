DataDog/dd-trace-dotnet

Small ASP.NET Snapshot refactoring (#1580)
* Update VerifyHelper to reorder tags in snapshots

Obviously, being a dictionary, the enumeration order is technically indeterminate, but in practice, this reordering seems to work, and is necessary where the order tags are added to the span at the source may change per invocation

* Move snapshots into a Snaphots sub folder

Lose the benefits of code folding in VS/Rider, but there's so many, it's too unwieldy within GitHub/filesystem currently

* Move ASP.NET Snapshots to sub folder

* Update aspnet snapshots to reorder tags and add missing runtime-id

* Fix directory char in samples directory

Uses correct character regardless of OS
This was causing some issues with IisExpress for me locally

* Don't pass executable AND applicationPath to StartProcess

I personally found this really confusing, especially when it comes to aspnetcore, and has clearly evolved a lot - there's two paths passed to it, sometimes one is executed, sometimes the other etc. Plus double negatives.
Instead, I pass the executable, add applicationPath to args if necessary in the callee, and explicitly allow passing the process to profile (optional). I think that makes things a bit clearer, but it could do with more refactoring, and reducing of duplication tbh.

* Allow specifying how many web server spans are expected in TestHelper

test/Datadog.Trace.TestHelpers/EnvironmentHelper.cs
@@ -39,12 +39,12 @@ namespace Datadog.Trace.TestHelpers
             string sampleName,
             Type anchorType,
             ITestOutputHelper output,
-            string samplesDirectory = "test/test-applications/integrations",
+            string samplesDirectory = null,
             bool prependSamplesToAppName = true,
             bool requiresProfiling = true)
         {
             SampleName = sampleName;
-            _samplesDirectory = samplesDirectory ?? "test/test-applications/integrations";
+            _samplesDirectory = samplesDirectory ?? Path.Combine("test", "test-applications", "integrations");
             _targetFramework = Assembly.GetAssembly(anchorType).GetCustomAttribute<TargetFrameworkAttribute>();
             _output = output;
             _requiresProfiling = requiresProfiling;
@@ -121,11 +121,9 @@ namespace Datadog.Trace.TestHelpers
             int agentPort,
             int aspNetCorePort,
             int? statsdPort,
-            string processPath,
             StringDictionary environmentVariables,
-            bool ignoreProfilerProcesses = false)
+            string processToProfile = null)
         {
-            var processName = processPath;
             string profilerEnabled = _requiresProfiling ? "1" : "0";
             string profilerPath;
 
@@ -146,8 +144,6 @@ namespace Datadog.Trace.TestHelpers
                 profilerPath = GetProfilerPath();
                 environmentVariables["COR_PROFILER_PATH"] = profilerPath;
                 environmentVariables["DD_DOTNET_TRACER_HOME"] = Path.GetDirectoryName(profilerPath);
-
-                processName = Path.GetFileName(processPath);
             }
 
             if (DebugModeEnabled)
@@ -155,9 +151,9 @@ namespace Datadog.Trace.TestHelpers
                 environmentVariables["DD_TRACE_DEBUG"] = "1";
             }
 
-            if (!ignoreProfilerProcesses)
+            if (!string.IsNullOrEmpty(processToProfile))
             {
-                environmentVariables["DD_PROFILER_PROCESSES"] = processName;
+                environmentVariables["DD_PROFILER_PROCESSES"] = Path.GetFileName(processToProfile);
             }
 
             string integrations = string.Join(";", GetIntegrationsFilePaths());

pjanotti/opentelemetry-dotnet-instrumentation

test/Datadog.Trace.TestHelpers/EnvironmentHelper.cs
@@ -39,12 +39,12 @@ namespace Datadog.Trace.TestHelpers
             string sampleName,
             Type anchorType,
             ITestOutputHelper output,
-            string samplesDirectory = "test/test-applications/integrations",
+            string samplesDirectory = null,
             bool prependSamplesToAppName = true,
             bool requiresProfiling = true)
         {
             SampleName = sampleName;
-            _samplesDirectory = samplesDirectory ?? "test/test-applications/integrations";
+            _samplesDirectory = samplesDirectory ?? Path.Combine("test", "test-applications", "integrations");
             _targetFramework = Assembly.GetAssembly(anchorType).GetCustomAttribute<TargetFrameworkAttribute>();
             _output = output;
             _requiresProfiling = requiresProfiling;
@@ -121,11 +121,9 @@ namespace Datadog.Trace.TestHelpers
             int agentPort,
             int aspNetCorePort,
             int? statsdPort,
-            string processPath,
             StringDictionary environmentVariables,
-            bool ignoreProfilerProcesses = false)
+            string processToProfile = null)
         {
-            var processName = processPath;
             string profilerEnabled = _requiresProfiling ? "1" : "0";
             string profilerPath;
 
@@ -146,8 +144,6 @@ namespace Datadog.Trace.TestHelpers
                 profilerPath = GetProfilerPath();
                 environmentVariables["COR_PROFILER_PATH"] = profilerPath;
                 environmentVariables["OTEL_DOTNET_TRACER_HOME"] = Path.GetDirectoryName(profilerPath);
-
-                processName = Path.GetFileName(processPath);
             }
 
             if (DebugModeEnabled)
@@ -155,9 +151,9 @@ namespace Datadog.Trace.TestHelpers
                 environmentVariables["OTEL_TRACE_DEBUG"] = "1";
             }
 
-            if (!ignoreProfilerProcesses)
+            if (!string.IsNullOrEmpty(processToProfile))
             {
-                environmentVariables["OTEL_PROFILER_PROCESSES"] = processName;
+                environmentVariables["OTEL_PROFILER_PROCESSES"] = Path.GetFileName(processToProfile);
             }
 
             string integrations = string.Join(";", GetIntegrationsFilePaths());