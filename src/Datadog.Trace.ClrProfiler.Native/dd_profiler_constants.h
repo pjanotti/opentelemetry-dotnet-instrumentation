DataDog/dd-trace-dotnet

Fix PInvokes to the native lib on non windows os. (#1612)
* Fixes PInvokeMap on non windows platforms.

* Add Call from Initialize method.

* Addressing PR comments

* Changes based in a PR comment.

src/Datadog.Trace.ClrProfiler.Native/dd_profiler_constants.h
@@ -34,15 +34,12 @@ const WSTRING env_vars_to_display[]{environment::tracing_enabled,
                                     environment::azure_app_services_cli_telemetry_profile_value};
 
 const WSTRING skip_assembly_prefixes[]{
-    WStr("Datadog.Trace"),
-    WStr("MessagePack"),
     WStr("Microsoft.AI"),
     WStr("Microsoft.ApplicationInsights"),
     WStr("Microsoft.Build"),
     WStr("Microsoft.CSharp"),
     WStr("Microsoft.Extensions"),
     WStr("Microsoft.Web.Compilation.Snapshots"),
-    WStr("Sigil"),
     WStr("System.Core"),
     WStr("System.Console"),
     WStr("System.Collections"),
@@ -55,7 +52,6 @@ const WSTRING skip_assembly_prefixes[]{
     WStr("System.Text"),
     WStr("System.Threading"),
     WStr("System.Xml"),
-    WStr("Newtonsoft"),
 };
 
 const WSTRING skip_assemblies[]{WStr("mscorlib"),
@@ -68,6 +64,10 @@ const WSTRING skip_assemblies[]{WStr("mscorlib"),
 
 const WSTRING managed_profiler_full_assembly_version =
     WStr("Datadog.Trace.ClrProfiler.Managed, Version=1.28.1.0, Culture=neutral, PublicKeyToken=def86d061d0d2eeb");
+
+const WSTRING managed_profiler_name = WStr("Datadog.Trace.ClrProfiler.Managed");
+
+const WSTRING nonwindows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+NonWindows");
 
 const WSTRING calltarget_modification_action = WStr("CallTargetModification");
 

pjanotti/opentelemetry-dotnet-instrumentation

src/Datadog.Trace.ClrProfiler.Native/dd_profiler_constants.h
@@ -34,16 +34,12 @@ const WSTRING env_vars_to_display[]{environment::tracing_enabled,
                                     environment::azure_app_services_cli_telemetry_profile_value};
 
 const WSTRING skip_assembly_prefixes[]{
-    WStr("Datadog.Trace"),
-    WStr("OpenTelemetry.AutoInstrumentation"),
-    WStr("MessagePack"),
     WStr("Microsoft.AI"),
     WStr("Microsoft.ApplicationInsights"),
     WStr("Microsoft.Build"),
     WStr("Microsoft.CSharp"),
     WStr("Microsoft.Extensions"),
     WStr("Microsoft.Web.Compilation.Snapshots"),
-    WStr("Sigil"),
     WStr("System.Core"),
     WStr("System.Console"),
     WStr("System.Collections"),
@@ -56,7 +52,6 @@ const WSTRING skip_assembly_prefixes[]{
     WStr("System.Text"),
     WStr("System.Threading"),
     WStr("System.Xml"),
-    WStr("Newtonsoft"),
 };
 
 const WSTRING skip_assemblies[]{WStr("mscorlib"),
@@ -69,6 +64,10 @@ const WSTRING skip_assemblies[]{WStr("mscorlib"),
 
 inline WSTRING managed_profiler_full_assembly_version =
     WStr("OpenTelemetry.AutoInstrumentation.ClrProfiler.Managed, Version=0.0.1.0, Culture=neutral, PublicKeyToken=34b8972644a12429");
+
+const WSTRING managed_profiler_name = WStr("OpenTelemetry.AutoInstrumentation.ClrProfiler.Managed");
+
+const WSTRING nonwindows_nativemethods_type = WStr("Datadog.Trace.ClrProfiler.NativeMethods+NonWindows");
 
 const WSTRING calltarget_modification_action = WStr("CallTargetModification");
 