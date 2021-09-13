DataDog/dd-trace-dotnet

Logger API refactoring (#1625)
* Move logging free functions (Info, Debug, Warn) to static function member of Logger class
* Rewrite the logger implementation to allow one logger for the Tracer, for the Profiler,
  for the Common librairies, and more...

src/Datadog.Trace.ClrProfiler.Native/logger.h
@@ -1,0 +1,88 @@
+#pragma once
+
+#include "environment_variables.h"
+#include "string.h"
+#include "logger_impl.h"
+
+#include <string>
+
+
+namespace trace
+{
+
+struct TracerLoggerPolicy
+{
+    inline static const std::string file_name = "dotnet-tracer-native";
+#ifdef _WIN32
+    // this field will be removed once merged with the profiler in order to have
+    // the same product folder name
+    inline static const WSTRING folder_path = WStr(R"(Datadog .NET Tracer\logs)");
+#endif
+    inline static const std::string pattern = "%D %I:%M:%S.%e %p [%P|%t] [%l] %v";
+    struct logging_environment
+    {
+        // cannot reuse environment::log_path variable. On alpine, test fails
+        inline static const WSTRING log_path = WStr("DD_TRACE_LOG_PATH");
+    };
+};
+
+class Logger
+{
+private:
+    Logger() = delete;
+    Logger(Logger&) = delete;
+    Logger(Logger&&) = delete;
+    Logger& operator=(Logger&) = delete;
+    Logger& operator=(Logger&&) = delete;
+
+public:
+    template <typename... Args>
+    static void Debug(const Args&... args)
+    {
+        LoggerImpl<TracerLoggerPolicy>::Instance()->Debug(args...);
+    }
+
+    template <typename... Args>
+    static void Info(const Args&... args)
+    {
+        LoggerImpl<TracerLoggerPolicy>::Instance()->Info(args...);
+    }
+
+    template <typename... Args>
+    static void Warn(const Args&... args)
+    {
+        LoggerImpl<TracerLoggerPolicy>::Instance()->Warn(args...);
+    }
+    template <typename... Args>
+    static void Error(const Args&... args)
+    {
+        LoggerImpl<TracerLoggerPolicy>::Instance()->Error(args...);
+    }
+    template <typename... Args>
+    static void Critical(const Args&... args)
+    {
+        LoggerImpl<TracerLoggerPolicy>::Instance()->Critical(args...);
+    }
+
+    static void EnableDebug()
+    {
+        LoggerImpl<TracerLoggerPolicy>::Instance()->EnableDebug();
+    }
+
+    static bool IsDebugEnabled()
+    {
+        return LoggerImpl<TracerLoggerPolicy>::Instance()->IsDebugEnabled();
+    }
+
+    static void Shutdown()
+    {
+        LoggerImpl<TracerLoggerPolicy>::Shutdown();
+    }
+
+    static void Flush()
+    {
+        LoggerImpl<TracerLoggerPolicy>::Instance()->Flush();
+    }
+};
+
+} // namespace trace

src/Datadog.Trace.ClrProfiler.Native/pal.h
@@ -25,8 +25,11 @@
 namespace trace
 {
 
-inline WSTRING DatadogLogFilePath(const std::string& file_name_suffix)
+template <class TLoggerPolicy>
+inline WSTRING GetDatadogLogFilePath(const std::string& file_name_suffix)
 {
+    const auto file_name = TLoggerPolicy::file_name + file_name_suffix + ".log";
+
     WSTRING directory = GetEnvironmentValue(environment::log_directory);
 
     if (directory.length() > 0)
@@ -37,10 +40,10 @@ inline WSTRING DatadogLogFilePath(const std::string& file_name_suffix)
 #else
                WStr('/') +
 #endif
-               ToWSTRING("dotnet-tracer-native" + file_name_suffix + ".log");
+               ToWSTRING(file_name);
     }
 
-    WSTRING path = GetEnvironmentValue(environment::log_path);
+    WSTRING path = GetEnvironmentValue(TLoggerPolicy::logging_environment::log_path);
 
     if (path.length() > 0)
     {
@@ -48,23 +51,17 @@ inline WSTRING DatadogLogFilePath(const std::string& file_name_suffix)
     }
 
 #ifdef _WIN32
-    char* p_program_data;
-    size_t length;
-    const errno_t result = _dupenv_s(&p_program_data, &length, "PROGRAMDATA");
-    std::string program_data;
-
-    if (SUCCEEDED(result) && p_program_data != nullptr && length > 0)
+    WSTRING program_data = GetEnvironmentValue(WStr("PROGRAMDATA"));
+    std::filesystem::path program_data_path;
+    if (program_data.length() == 0)
     {
-        program_data = std::string(p_program_data);
-    }
-    else
-    {
-        program_data = R"(C:\ProgramData)";
+        program_data_path = WStr(R"(C:\ProgramData)");
     }
 
-    return ToWSTRING(program_data + R"(\Datadog .NET Tracer\logs\dotnet-tracer-native)" + file_name_suffix + ".log");
+    // on Windows WSTRING == wstring
+    return (program_data_path / TLoggerPolicy::folder_path  / file_name).wstring();
 #else
-    return ToWSTRING("/var/log/datadog/dotnet/dotnet-tracer-native" + file_name_suffix + ".log");
+    return ToWSTRING("/var/log/datadog/dotnet/" + file_name);
 #endif
 }
 

pjanotti/opentelemetry-dotnet-instrumentation

src/Datadog.Trace.ClrProfiler.Native/logger.h
@@ -1,0 +1,88 @@
+#pragma once
+
+#include "environment_variables.h"
+#include "string.h"
+#include "logger_impl.h"
+
+#include <string>
+
+
+namespace trace
+{
+
+struct TracerLoggerPolicy
+{
+    inline static const std::string file_name = "dotnet-tracer-native";
+#ifdef _WIN32
+    // this field will be removed once merged with the profiler in order to have
+    // the same product folder name
+    inline static const WSTRING folder_path = WStr(R"(OpenTelemetry .NET AutoInstrumentation\logs)");
+#endif
+    inline static const std::string pattern = "%D %I:%M:%S.%e %p [%P|%t] [%l] %v";
+    struct logging_environment
+    {
+        // cannot reuse environment::log_path variable. On alpine, test fails
+        inline static const WSTRING log_path = WStr("OTEL_TRACE_LOG_PATH");
+    };
+};
+
+class Logger
+{
+private:
+    Logger() = delete;
+    Logger(Logger&) = delete;
+    Logger(Logger&&) = delete;
+    Logger& operator=(Logger&) = delete;
+    Logger& operator=(Logger&&) = delete;
+
+public:
+    template <typename... Args>
+    static void Debug(const Args&... args)
+    {
+        LoggerImpl<TracerLoggerPolicy>::Instance()->Debug(args...);
+    }
+
+    template <typename... Args>
+    static void Info(const Args&... args)
+    {
+        LoggerImpl<TracerLoggerPolicy>::Instance()->Info(args...);
+    }
+
+    template <typename... Args>
+    static void Warn(const Args&... args)
+    {
+        LoggerImpl<TracerLoggerPolicy>::Instance()->Warn(args...);
+    }
+    template <typename... Args>
+    static void Error(const Args&... args)
+    {
+        LoggerImpl<TracerLoggerPolicy>::Instance()->Error(args...);
+    }
+    template <typename... Args>
+    static void Critical(const Args&... args)
+    {
+        LoggerImpl<TracerLoggerPolicy>::Instance()->Critical(args...);
+    }
+
+    static void EnableDebug()
+    {
+        LoggerImpl<TracerLoggerPolicy>::Instance()->EnableDebug();
+    }
+
+    static bool IsDebugEnabled()
+    {
+        return LoggerImpl<TracerLoggerPolicy>::Instance()->IsDebugEnabled();
+    }
+
+    static void Shutdown()
+    {
+        LoggerImpl<TracerLoggerPolicy>::Shutdown();
+    }
+
+    static void Flush()
+    {
+        LoggerImpl<TracerLoggerPolicy>::Instance()->Flush();
+    }
+};
+
+} // namespace trace

src/Datadog.Trace.ClrProfiler.Native/pal.h
@@ -25,8 +25,11 @@
 namespace trace
 {
 
-inline WSTRING DatadogLogFilePath(const std::string& file_name_suffix)
+template <class TLoggerPolicy>
+inline WSTRING GetDatadogLogFilePath(const std::string& file_name_suffix)
 {
+    const auto file_name = TLoggerPolicy::file_name + file_name_suffix + ".log";
+
     WSTRING directory = GetEnvironmentValue(environment::log_directory);
 
     if (directory.length() > 0)
@@ -37,10 +40,10 @@ inline WSTRING DatadogLogFilePath(const std::string& file_name_suffix)
 #else
                WStr('/') +
 #endif
-               ToWSTRING("dotnet-tracer-native" + file_name_suffix + ".log");
+               ToWSTRING(file_name);
     }
 
-    WSTRING path = GetEnvironmentValue(environment::log_path);
+    WSTRING path = GetEnvironmentValue(TLoggerPolicy::logging_environment::log_path);
 
     if (path.length() > 0)
     {
@@ -48,23 +51,17 @@ inline WSTRING DatadogLogFilePath(const std::string& file_name_suffix)
     }
 
 #ifdef _WIN32
-    char* p_program_data;
-    size_t length;
-    const errno_t result = _dupenv_s(&p_program_data, &length, "PROGRAMDATA");
-    std::string program_data;
-
-    if (SUCCEEDED(result) && p_program_data != nullptr && length > 0)
+    WSTRING program_data = GetEnvironmentValue(WStr("PROGRAMDATA"));
+    std::filesystem::path program_data_path;
+    if (program_data.length() == 0)
     {
-        program_data = std::string(p_program_data);
-    }
-    else
-    {
-        program_data = R"(C:\ProgramData)";
+        program_data_path = WStr(R"(C:\ProgramData)");
     }
 
-    return ToWSTRING(program_data + R"(\OpenTelemetry .NET AutoInstrumentation\logs\dotnet-tracer-native)" + file_name_suffix + ".log");
+    // on Windows WSTRING == wstring
+    return (program_data_path / TLoggerPolicy::folder_path  / file_name).wstring();
 #else
-    return ToWSTRING("/var/log/opentelemetry/dotnet/dotnet-tracer-native" + file_name_suffix + ".log");
+    return ToWSTRING("/var/log/opentelemetry/dotnet/" + file_name);
 #endif
 }
 