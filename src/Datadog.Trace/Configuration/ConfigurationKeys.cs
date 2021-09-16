DataDog/dd-trace-dotnet

Disable ILogger integration by default (#1738)
* Disable ILogger integration by default

In order to better verify the performance impact of the ILogger integration, disable the integration by default, by adding an additional feature flag.

* Fix configuration tests broken by disabling ilogger by default

src/Datadog.Trace/Configuration/ConfigurationKeys.cs
@@ -396,6 +396,11 @@ namespace Datadog.Trace.Configuration
             /// Feature Flag: enables instrumenting calls to netstandard.dll (only applies to CallSite instrumentation)
             /// </summary>
             public const string NetStandardEnabled = "DD_TRACE_NETSTANDARD_ENABLED";
+
+            /// <summary>
+            /// Feature Flag: enables the ILogger logs injection integration. Defaults to false.
+            /// </summary>
+            public const string ILoggerIntegrationEnabled = "DD_TRACE_ILOGGER_LOGS_INJECTION_ENABLED";
         }
     }
 }

pjanotti/opentelemetry-dotnet-instrumentation

src/Datadog.Trace/Configuration/ConfigurationKeys.cs
@@ -435,6 +435,11 @@ namespace Datadog.Trace.Configuration
             /// Feature Flag: enables instrumenting calls to netstandard.dll (only applies to CallSite instrumentation)
             /// </summary>
             public const string NetStandardEnabled = "OTEL_TRACE_NETSTANDARD_ENABLED";
+
+            /// <summary>
+            /// Feature Flag: enables the ILogger logs injection integration. Defaults to false.
+            /// </summary>
+            public const string ILoggerIntegrationEnabled = "OTEL_TRACE_ILOGGER_LOGS_INJECTION_ENABLED";
         }
     }
 }